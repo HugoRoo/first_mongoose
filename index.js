eb6ab04d9cedd9462156ae952cd9f9198a8a926d
HugoRoo <88414687+HugoRoo@users.noreply.github.com>
2022-02-23 21:54:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 
 
-const DB_USER = 'hugoRoo'
-const DB_PASSWORD = '123Tamarindo'
-const DB_HOST = 'kodemia16.5djum.mongodb.net'
-const DB_NAME = 'kodemia' 
+//password to conection
 
 const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
 
